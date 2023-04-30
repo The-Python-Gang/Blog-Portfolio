50f61e82660acd9b62219748d2841b48f23965bc
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-04-30 19:51:13 -0300

    done: start project

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, re_path
+from portfolio import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', views.index)
 ]
