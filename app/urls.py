cafcec120dbdf8f3377c2617adf54c583b910215
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-05-16 15:16:04 -0300

    feat: add template list posts

--- app/urls.py
+++ app/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        path('', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+
+def post_list(request):
+    template_name = 'post-list.html'
+    posts = Post.objects.all()
+    context = {
+        'posts' : posts
+    }
+    return render(request, template_name, context)
